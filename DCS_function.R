27995e61c7a26719e79933c49dca053621305611
wayne0831 <wyane0831@skku.edu>
2019-09-24 23:05:50 +0900

    Doc_predict function inserted into DCS_function.R

--- DCS_function.R
+++ DCS_function.R
@@ -177,3 +177,54 @@ create_dtm <- function(df){
   
   return (result) }
 
+Doc_predict <- function(folder_list = folder_list){
+  
+  # train data frame
+  df.tmp <- data.frame()
+  
+  for (i in 1:NROW(folder_list)){
+    folder_path <- paste0(root_folder, "/", folder_list[i], "/")
+    tmp         <- get_folder_info(folder_path)
+    df.tmp      <- rbind(df.tmp, tmp)}
+  
+  df.tmp$doc_id <- 1:NROW(df.tmp)
+  df.tmp        <- df.tmp %>% select(doc_id, Path, Type, Name, Extension, text)
+  
+  # Create the document term matrix and Remove the doc_id column
+  df.tmp        <- create_dtm(df.tmp)
+  df.tmp        <- df.tmp[, -1]
+  
+  # rearrange the column of df.tmp to equalize with dtm.all
+  order <- which(colnames(df.tmp) == colnames(dtm.all[,-1])[1])
+  for(i in 2:40){ order <- cbind(order, which(colnames(df.tmp) == colnames(dtm.all[,-1])[i])) }
+  
+  # set df.tmp as test data
+  test <- df.tmp[, order]
+  
+  d.t  <- dtm.all[, -1]
+  tmp  <- test
+  a    <- data.frame()
+  for (i in 1:ncol(tmp)) {if(tmp[ , i]!=0){a <- c(a, i)}}
+  
+  for(j in 1:(ncol(d.t)-1)){
+    if (j %in% a) {d.t <- d.t[which(d.t[, j]!= 0), ]} 
+    else          {d.t <- d.t[which(d.t[, j]== 0), ]}
+  }
+  
+  test <- d.t[1, ]
+  
+  # load the result of Stack_XGB 
+  test_st     <- data.frame(Type        = test$Type,
+                            tr_predict  = predict(dt_fit, newdata = test),
+                            rf_predict  = predict(rf_fit2, newdata = test),
+                            xgb_predict = predict(xgb_fit, newdata = test),
+                            svm_predict = predict(tuned.svm, newdata = test),
+                            mdl_predict = predict(mdl_fit, newdata = test),
+                            bag_predict = predict(bag_fit, newdata = test))
+  
+  result <- list(Prediction = predict(xgb_st, test_st),
+                 Actual     = test_st$Type)
+  
+  return(result)
+}
+
